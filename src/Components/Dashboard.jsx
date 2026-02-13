3f00bd3d93653d18d642842ea9a3fb4c55c8ccdb
Pratik1618 <kadampratik18@gmail.com>
2026-02-13 13:40:36 +0530

    font changes in form selected

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -322,8 +322,8 @@ const filteredFiles = useMemo(() => {
       </div>
          {selectedFileName ? (
               <div className="mt-2 rounded-lg border border-slate-200 bg-slate-100 px-3 py-2 text-xs text-slate-700">
-                <span className="font-medium text-slate-600">Selected form:</span>{" "}
-                <span className="break-all">{selectedCategory}/ {selectedFileName}</span>
+                <span className="font-semibold  text-slate-600">Selected form:</span>{" "}
+                <span className="font-semibold break-all">{selectedCategory}/ {selectedFileName}</span>
               </div>
             ) : null}
     </div>
